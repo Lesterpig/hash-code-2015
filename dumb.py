277fd8789a5a9d5870cf269fb0f53a81fb03135c
Quentin Dufour <quentin@dufour.tk>
2015-03-12 19:57:09 +0100

    Step 1 : order by c/t

--- dumb.py
+++ dumb.py
@@ -12,10 +12,16 @@ servers = list()
 
 for b in range(bannedPlacementNb):
   ban = input().split(" ")
-  bannedPlacement.append([int(ban[0]), int(ban[1])])
+  bannedPlacement.append((int(ban[0]), int(ban[1])))
 
 for s in range(serverNb):
   serv = input().split(" ")
-  servers.append([int(serv[0]), int(serv[1])])
+  # Create a tuple with size, capacity, and capacity/size
+  servers.append((int(serv[0]), int(serv[1]), int(serv[1]) / int(serv[0])))
 
+#Order by capacity/size dec
+servers = sorted(servers, key=lambda servers: servers[2], reverse=True)
 
+#Log servers ordered
+#for s in range(serverNb):
+  #print(servers[s])
